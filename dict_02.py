c7bbc2c5b250ab44186b7bf4f90ad851403465be
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-06-25 19:15:36 -0300

    sixth commit

--- dict_02.py
+++ dict_02.py
@@ -28,13 +28,29 @@ print('DESTINO:', destino)
 print('CÓPIA:', copia) 
 print('Cópia rasa do dicionário (mesmo endereço para dados mutáveis, ex: lista\n')
 
-copia_profunda = copy.deepcopy(copia)
+copia_profunda = copy.deepcopy(copia) # cópia profunda atualiza todos os sub níveis
 copia_profunda['item_1'] = "cópia profunda"
 copia_profunda['item_2'] = "alterado de novo"
-copia_profunda["lista_1"][5] = -1
+copia_profunda["lista_1"][5] = -1 # somente esse dicionário teve a lista alterada
 
 print('ORIGEM:', origem)
 print('DESTINO:', destino)
 print('CÓPIA:', copia) 
 print('CÓPIA PROFUNDA:', copia_profunda) 
 print('Cópia profunda do dicionário e da lista em outro endereço de memória\n')
+
+destino.update({ # Atualiza um dicionário
+  'item_2': 'novamente alterado',
+  'item_3': 'novo valor',
+  'lista_2': [-10, -9, -8, -7, -6, -5],
+})
+print('DESTINO:', destino)
+print('Destino foi atualizado por update\n')
+
+destino.pop('item_3') # Remove um item do dicionário
+print('DESTINO:', destino)
+print('Destino teve um item removido por pop\n')
+
+ultimo = destino.popitem()
+print('DESTINO:', destino)
+print('último item remvodido:', ultimo, '\n')
