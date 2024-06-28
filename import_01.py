963d21abf81552663132296600ca75a862a648ec
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-06-27 23:00:56 -0300

    23th commit

--- import_01.py
+++ import_01.py
@@ -1,4 +1,4 @@
 from packages import module_01 as m01
 
 print('Este módulo (interno) se chama', __name__)
-print('Função do módulo externo: Soma 2 + 3 =' + str(m01.soma(2, 3)))
+print('Função do módulo externo: Soma 2 + 3 = ' + str(m01.soma(2, 3)))
