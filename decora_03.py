f511872dd552b3f42cdcb82e3927dc2056569be6
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-06-28 22:43:50 -0300

    35th commit

--- decora_03.py
+++ decora_03.py
@@ -1,10 +1,15 @@
+# Decorador com log de erros personalizados
+
 import logging, time, os
 
 def factory_errors(function):
-  print('Decorator called...')
-  print('\tOn function:', function.__name__)
-
+  # print('Decorator called...')
+  # print('\tOn function:', function.__name__)
+  
   def error_handler(code, description, level):
+    # print('\tInner function called...')
+    # print('\t\t', code, '\n\t\t', description, '\n\t\t', level)
+
     logging.basicConfig(filename='error.log', level=logging.ERROR)
     local_date = time.strftime("%Y-%m-%d", time.localtime())
     local_time = time.strftime("%H:%M:%S", time.localtime())
@@ -21,13 +26,22 @@ def factory_errors(function):
       os_pid,
       os_cwd
     ))    
-    logging.shutdown()  
+    logging.shutdown()
+    
+    try:
+      raise Exception ('Sorry, an error occurred: ' + str(code) + ' - ' + description + ' (' + level + ')')
+    except Exception as inst:
+      return inst
+    
   return error_handler
   
 @factory_errors
 def set_error(code, description,  level):
-  raise Exception(code, description, level)
+  ...
 
-set_error(300, "Custom Error", "My Error")
+# verbose mode
+print(set_error(300, "Custom Error", "My Error"))
+
+# silent mode
 set_error(400, 'Bad Request ',  'Client Error')
 set_error(500, 'Internal Server Error', 'Server Error')
