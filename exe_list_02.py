4408e7ead3194a2d0c8919de6e4af6e394d96b38
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-06-30 03:35:59 -0300

    40th commit

--- exe_list_02.py
+++ exe_list_02.py
@@ -1,9 +1,16 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+
+import json, os
+
+tasks = []
+tasks_to_redo = []
+filename = 'tasks.json'
+
 def show(tasks):
   print()
   if not tasks:
     print('no tasks to show!')
     return
-
   print('Tasks:')
   for task in tasks:
     print(f'\t{task}')
@@ -14,22 +21,22 @@ def undo(tasks, tasks_to_redo):
   if not tasks:
     print('No tasks to undo!')
     return
-
   task = tasks.pop()
   print(f'{task=} removed from the task list.')
   tasks_to_redo.append(task)
   print()
+  show(tasks)
 
 def redo(tasks, tasks_to_redo):
   print()
   if not tasks_to_redo:
     print('No tasks to redo!')
     return
-
   task = tasks_to_redo.pop()
   print(f'{task=} added to the task list.')
   tasks.append(task)
   print()
+  show(tasks)
 
 def add(tasks, task):
   print()
@@ -40,23 +47,50 @@ def add(tasks, task):
   print(f'{task=} added to the task list.')
   tasks.append(task)
   print()
-
-tasks = []
-tasks_to_redo = []
-
-while True:
-  print('Commands: show, undo, redo and quit')
-  task = input('Enter a task or command: ')
-  if task == 'undo':
-    undo(tasks, tasks_to_redo)
-  elif task == 'redo':
-    redo(tasks, tasks_to_redo)
-  elif task == 'quit':
-    break
-  else:
-    add(tasks, task)
- 
   show(tasks)
 
-print("Bye!")  
+def load(path):
+  global tasks
+  data = []
+  try:
+    with open(path, 'r', encoding='utf8') as file:
+      data = json.load(file)
+      tasks = data
+      print()
+      print("file was loaded!")
+  except FileNotFoundError:
+    print()
+    print('File not found!')
+
+def save(tasks, path):
+  data = tasks
+  with open(path, 'w', encoding = 'utf8') as file:
+    data = json.dump(tasks, file, indent=2, ensure_ascii=False)
+  tasks = data
+  print()
+  print("file was saved!")
+
+def quit():
+  print("Bye!")
+  print()  
+  os.system(exit())
+
+while True:
+  print('Commands: show, undo, redo, load save and quit')
+  task = input('Enter a task or command: ')
+  commands= {
+    'show' : lambda: show(tasks),
+    'undo' : lambda: undo(tasks, tasks_to_redo),
+    'redo' : lambda: redo(tasks, tasks_to_redo),
+    'add'  : lambda: add(tasks, task),
+    'load' : lambda: load(filename),
+    'save' : lambda: save(tasks, filename),
+    'quit' : lambda: quit()
+
+  }
+  
+  command = commands.get(task) if commands.get(task) is not None else \
+  commands['add']
+  command()
+
   
