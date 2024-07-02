7ba9d6340abb6e55cece3d3c0544e4f2d852d473
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-02 00:46:06 -0300

    47th commit

--- class_03.py
+++ class_03.py
@@ -44,12 +44,12 @@ class Classe:
   def funcao_que_esta_na_classe(*args, **kwargs):
     print('Oi', args, kwargs)
 
-  def funcao(*args, **kwargs):
-    print('Oi', args, kwargs)
+def funcao(*args, **kwargs):
+  print('Oi', args, kwargs)
 
 c1 = Classe()
 c1.funcao_que_esta_na_classe(1, 2, 3)
-c1.funcao(1, 2, 3)
+funcao(1, 2, 3)
 
 Classe.funcao_que_esta_na_classe(nomeado=1)
-c1.funcao(nomeado=1)
+funcao(nomeado=1)
