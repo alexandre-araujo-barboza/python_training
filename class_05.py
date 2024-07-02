22c7099ef1bd557c37fba4e58fdd1537cfe719c8
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-02 02:53:22 -0300

    51th commit

--- class_05.py
+++ class_05.py
@@ -17,8 +17,6 @@
 #       só DEVE ser usado na classe em que foi
 #       declarado.
 
-from functools import partial
-
 class Foo:
   def __init__(self):
     self.public = 'isso é público'
@@ -47,3 +45,47 @@ f = Foo()
 print(f.metodo_publico() + '\n') # Permitido
 print(f'Não usar fora da classe:  {f._metodo_protected()}')   # Não recomendado
 print(f'Não usar fora da classe: {f._Foo__metodo_private()}') # Não recomendado mesmo!
+
+# Relações entre classes: associação, agregação e composição
+# Associação é um tipo de relação onde os objetos
+# estão ligados dentro do sistema.
+# Essa é a relação mais comum entre objetos e tem subconjuntos
+# como agregação e composição (que veremos depois).
+# Geralmente, temos uma associação quando um objeto tem
+# um atributo que referencia outro objeto.
+# A associação não especifica como um objeto controla
+# o ciclo de vida de outro objeto.
+
+class Escritor:
+  def __init__(self, nome) -> None:
+    self.nome = nome
+    self._ferramenta = None
+
+  @property
+  def ferramenta(self):
+    return self._ferramenta
+
+  @ferramenta.setter
+  def ferramenta(self, ferramenta):
+    self._ferramenta = ferramenta
+
+class FerramentaDeEscrever:
+  def __init__(self, nome):
+    self.nome = nome
+
+  def escrever(self):
+    return f'{self.nome} está escrevendo'
+
+escritor = Escritor('Alexandre')
+caneta = FerramentaDeEscrever('Caneta Bic')
+maquina_de_escrever = FerramentaDeEscrever('Máquina')
+
+print()
+escritor.ferramenta = maquina_de_escrever
+print(maquina_de_escrever.escrever())
+print(f'Escritor ({escritor.nome}) usando: ({escritor.ferramenta.escrever()})')
+
+print()
+escritor.ferramenta = caneta
+print(caneta.escrever())
+print(f'Escritor ({escritor.nome}) usando: ({escritor.ferramenta.escrever()})')
