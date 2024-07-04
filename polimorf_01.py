9e7bb230d40190d38c08759a0b2aa2eb4a079fbe
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-04 01:15:32 -0300

    57th commit

--- polimorf_01.py
+++ polimorf_01.py
@@ -23,33 +23,29 @@ from abc import ABC, abstractmethod
 
 
 class Notificacao(ABC):
-    def __init__(self, mensagem):
-        self.mensagem = mensagem
+  def __init__(self, mensagem):
+    self.mensagem = mensagem
 
-    @abstractmethod
-    def enviar(self) -> bool: ...
-
+  @abstractmethod
+  def enviar(self) -> bool: ...
 
 class NotificacaoEmail(Notificacao):
-    def enviar(self) -> bool:
-        print('E-mail: enviando - ', self.mensagem)
-        return True
-
+  def enviar(self) -> bool:
+    print('E-mail: enviando - ', self.mensagem)
+    return True
 
 class NotificacaoSMS(Notificacao):
-    def enviar(self) -> bool:
-        print('SMS: enviando - ', self.mensagem)
-        return False
-
+  def enviar(self) -> bool:
+    print('SMS: enviando - ', self.mensagem)
+    return False
 
 def notificar(notificacao: Notificacao):
-    notificacao_enviada = notificacao.enviar()
+  notificacao_enviada = notificacao.enviar()
 
-    if notificacao_enviada:
-        print('Notificação enviada')
-    else:
-        print('Notificação NÃO enviada')
-
+  if notificacao_enviada:
+    print('Notificação enviada')
+  else:
+    print('Notificação NÃO enviada')
 
 notificacao_email = NotificacaoEmail('testando e-mail')
 notificar(notificacao_email)
