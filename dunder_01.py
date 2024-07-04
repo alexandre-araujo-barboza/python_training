4eb572538e66a70c8d0cd3f987d6b32558804563
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-04 01:56:41 -0300

    58th commit

--- dunder_01.py
+++ dunder_01.py
@@ -16,17 +16,17 @@
 # __str__(self) - str
 # __repr__(self) - str
 class Ponto:
-    def __init__(self, x, y, z='String'):
-        self.x = x
-        self.y = y
-        self.z = z
+  def __init__(self, x, y, z='String'):
+    self.x = x
+    self.y = y
+    self.z = z
 
-    def __str__(self):
-        return f'({self.x}, {self.y})'
+  def __str__(self):
+    return f'({self.x}, {self.y})'
 
-    def __repr__(self):
-        class_name = type(self).__name__
-        return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
+  def __repr__(self):
+    class_name = type(self).__name__
+    return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
 
 
 p1 = Ponto(1, 2)
