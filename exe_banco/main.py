56b944376ebf284b3f2bcd8f87beda9edb390fa0
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-04 05:45:51 -0300

    66th commit

--- exe_banco/main.py
+++ exe_banco/main.py
@@ -3,15 +3,19 @@ Exercício com Abstração, Herança, Encapsulamento e Polimorfismo
 Criar um sistema bancário (extremamente simples) que tem clientes, contas e
 um banco. A ideia é que o cliente tenha uma conta (poupança ou corrente) e que
 possa sacar/depositar nessa conta. Contas corrente tem um limite extra.
+
 Conta (ABC)
     ContaCorrente
     ContaPoupanca
-Pessoa (ABC)
+
+Pessoa
     Cliente
-        Clente -> Conta
+        Clente -> Conta (um para um ou um para muitos)
+
 Banco
     Banco -> Cliente
     Banco -> Conta
+
 Dicas:
 Criar classe Cliente que herda da classe Pessoa (Herança)
     Pessoa tem nome e idade (com getters)
@@ -24,28 +28,29 @@ Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
     polimorfismo - as subclasses que implementam o método sacar)
 Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
 Banco será responsável autenticar o cliente e as contas da seguinte maneira:
-    Banco tem contas e clentes (Agregação)
+    Banco tem contas e clientes (Agregação)
     * Checar se a agência é daquele banco
     * Checar se o cliente é daquele banco
     * Checar se a conta é daquele banco
 Só será possível sacar se passar na autenticação do banco (descrita acima)
-Banco autentica por um método.
+Banco autentica por um método (autenticar).
 """
+import contas
+import pessoas
+from banco import Banco
 
-from package import module_05 as m05
+c1 = pessoas.Cliente('Luiz', 30)
+cc1 = contas.ContaCorrente(111, 222, 0, 0)
+c1.conta = cc1
+c2 = pessoas.Cliente('Maria', 18)
+cp1 = contas.ContaPoupanca(112, 223, 100)
+c2.conta = cp1
+banco = Banco()
+banco.clientes.extend([c1, c2])
+banco.contas.extend([cc1, cp1])
+banco.agencias.extend([111, 222])
 
-if __name__ == '__main__':
-  cp1 = m05.ContaPoupanca(111, 222)
-  cp1.sacar(1)
-  cp1.depositar(1)
-  cp1.sacar(1)
-  cp1.sacar(1)
-  print('##')
-  cc1 = m05.ContaCorrente(111, 222, 0, 100)
-  cc1.sacar(1)
-  cc1.depositar(1)
-  cc1.sacar(1)
-  cc1.sacar(1)
-  cc1.sacar(98)
-  cc1.sacar(1)
-  print('##')
+if banco.autenticar(c1, cc1):
+  cc1.depositar(10)
+  c1.conta.depositar(100)
+  print(c1.conta)
