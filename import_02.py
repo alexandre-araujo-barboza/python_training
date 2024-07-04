3007fc808ebf6333061cef132d6f451cf60a443b
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-04 04:17:10 -0300

    64th commit

---
+++ import_02.py
@@ -0,0 +1,9 @@
+from package import module_04 as m04
+
+print(dir(m04))
+print('Doc:',  m04.__doc__)
+print('File:', m04.__file__)
+print('Name:', m04.__name__)
+
+help(m04)
+    
---
+++ package/module_04.py
@@ -0,0 +1,14 @@
+
+# Recomendado usar aspas duplas
+
+"""
+ISSO É UM DOCSTRIG DE VÁRIAS LINHAS
+Obs.:
+    Documentação do módulo
+    O que esse módulo vai fazer?
+"""
+
+variavel = 'teste'
+def function():
+  return 'teste'
+
