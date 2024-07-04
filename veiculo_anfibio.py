d92486aefb950624dc83433de65fc7da8e683fd4
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-04 11:25:28 -0300

    67th commit

--- veiculo_anfibio.py
+++ veiculo_anfibio.py
@@ -17,14 +17,14 @@ class Veiculo(abc.ABC):
       results = func(*args, **kwargs)
       print('Veiculo chamado com:')
       for i, arg in enumerate(args):
-        print(f'\targ_{i}: {arg}')
+        print(f'\targ_{i+1}: {arg}')
       return results
     return inner
   
-  def __new__(cls, *args, **kwargs):
+  def __new__(cls, *args):
     print('Veiculo novo com:')
-    for i, arg in enumerate(args):
-      print(f'\targ_{i}: {arg}')
+    for arg in args:
+      print(f'\targumento: {arg}')
     instance = super().__new__(cls)
     return instance
   
@@ -40,7 +40,10 @@ class Veiculo(abc.ABC):
   
   def __exit__(self, terrain) -> None:
     print(f'Veículo saiu da: {terrain}')
-    
+  
+  def __del__(self) -> None:
+    print('Destruindo veículo...')
+
   @abc.abstractmethod
   def ligar(self) -> None:
     print('veículo ligando...')
@@ -116,16 +119,16 @@ class Carro(Veiculo) :
   def __call__(self, func):
     return super().__call__(func)
    
-  def __new__(cls, *args, **kwargs):
-    return super().__new__(cls, args, kwargs)
+  def __new__(cls, *args):
+    return super().__new__(cls, args)
   
   def __repr__(self):
     return super().__repr__()
   
-  def __enter__(self, terrain) -> None:
+  def __enter__(self, terrain='terra') -> None:
     super().__enter__(terrain)
   
-  def __exit__(self, terrain) -> None:
+  def __exit__(self, terrain='terra') -> None:
     super().__exit__(terrain)
     
   def ligar(self) -> None:
@@ -147,16 +150,16 @@ class Barco(Veiculo) :
   def __call__(self, func):
     return super().__call__(func)
    
-  def __new__(cls, *args, **kwargs):
-    return super().__new__(cls, args, kwargs)
+  def __new__(cls, *args):
+    return super().__new__(cls, args)
   
   def __repr__(self):
     return super().__repr__()
   
-  def __enter__(self, terrain) -> None:
+  def __enter__(self, terrain='água') -> None:
     super().__enter__(terrain)
   
-  def __exit__(self, terrain) -> None:
+  def __exit__(self, terrain='água') -> None:
     super().__exit__(terrain)
     
   def ligar(self) -> None:
@@ -177,8 +180,8 @@ class Anfibio(Carro, Barco):
   def __call__(self, func):
     return super().__call__(func)
    
-  def __new__(cls, *args, **kwargs):
-    return super().__new__(cls, args, kwargs)
+  def __new__(cls, *args):
+    return super().__new__(cls, args)
   
   def __repr__(self):
     return super().__repr__()
@@ -201,34 +204,39 @@ class Anfibio(Carro, Barco):
   def desligar(self) -> None:
     super().desligar()
 
-# carro = Carro(185, 15.2, 'TOYOTA', 'Mobile Flex')
-# carro.__repr__()
-# carro.__enter__('terra')
-# carro.ligar()
-# carro.velocidade = 100
-# carro.rpm = 3000
-# carro.acelerar(carro.velocidade, carro.rpm)
-# carro.velocidade = 50
-# carro.rpm = 2000
-# carro.freiar(carro.velocidade, carro.rpm)
-# carro.desligar()
-# carro.__exit__('terra')
+print('\nVeículo é um carro.')
+carro = Carro(200, 18.0, 'Mercedez', 'C-500')
+carro.__call__(carro.__repr__())
+carro.ligar()
+carro.__enter__()
+carro.velocidade = 140
+carro.rpm = 4100
+carro.acelerar(carro.velocidade, carro.rpm)
+carro.velocidade = 64
+carro.rpm = 2180
+carro.freiar(carro.velocidade, carro.rpm)
+carro.__exit__()
+carro.desligar()
+del carro
 
-# barco = Barco(185, 15.2, 'TOYOTA', 'Mobile Flex')
-# barco.__repr__()
-# barco.__enter__('água')
-# barco.ligar()
-# barco.velocidade = 100
-# barco.rpm = 3000
-# barco.acelerar(carro.velocidade, carro.rpm)
-# barco.velocidade = 50
-# barco.rpm = 2000
-# barco.freiar(carro.velocidade, carro.rpm)
-# barco.desligar()
-# barco.__exit__('água')
+print('\nVeículo é um barco.')
+barco = Barco(160, 11.7, 'Volvo', 'V-30')
+barco.__call__(barco.__repr__())
+barco.ligar()
+barco.__enter__()
+barco.velocidade = 80
+barco.rpm = 2200
+barco.acelerar(barco.velocidade, barco.rpm)
+barco.velocidade = 40
+barco.rpm = 1650
+barco.freiar(barco.velocidade, barco.rpm)
+barco.__exit__()
+barco.desligar()
+del barco
 
-anfibio = Anfibio(185, 15.2, 'TOYOTA', 'Mobile Flex')
-anfibio.__repr__()
+print('\nVeículo é anfíbio.')
+anfibio = Anfibio(185, 15.2, 'Toyota', 'Mobiflex')
+anfibio.__call__(anfibio.__repr__())
 anfibio.ligar()
 anfibio.__enter__('terra')
 anfibio.velocidade = 120
@@ -247,3 +255,4 @@ anfibio.rpm = 3000
 anfibio.acelerar(anfibio.velocidade, anfibio.rpm)
 anfibio.__exit__('água')
 anfibio.desligar()
+del anfibio
