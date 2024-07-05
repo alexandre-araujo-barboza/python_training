436ddb282c16ef188dcedcf43e03fe72a71cd7ee
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-04 23:15:46 -0300

    73th commit

---
+++ datetime_01.py
@@ -0,0 +1,40 @@
+# Criando datas com módulo datetime
+# datetime(ano, mês, dia)
+# datetime(ano, mês, dia, horas, minutos, segundos)
+# datetime.strptime('DATA', 'FORMATO')
+# datetime.now()
+# https://pt.wikipedia.org/wiki/Era_Unix
+# datetime.fromtimestamp(Unix Timestamp)
+# https://docs.python.org/3/library/datetime.html
+# Para timezones
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# Instalando o pytz
+# pip install pytz types-pytz
+
+from datetime import datetime
+from pytz import timezone
+
+data_str_data = '2022/04/20 07:49:23'
+data_str_data = '20/04/2022'
+data_str_fmt = '%d/%m/%Y'
+
+data = datetime.strptime(data_str_data, data_str_fmt)
+print(data)
+
+data = datetime.now()
+print(data.timestamp())  # Isso está na base de dados
+print(datetime.fromtimestamp(1670849077))
+
+data = datetime(2022, 4, 20, 7, 49, 23, tzinfo=timezone('Asia/Tokyo'))
+print(data)
+
+data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S')
+print(data.strftime('%d/%m/%Y'))
+print(data.strftime('%d/%m/%Y %H:%M'))
+print(data.strftime('%d/%m/%Y %H:%M:%S'))
+print(data.strftime('%Y'), data.year)
+print(data.strftime('%d'), data.day)
+print(data.strftime('%m'), data.month)
+print(data.strftime('%H'), data.hour)
+print(data.strftime('%M'), data.minute)
+print(data.strftime('%S'), data.second)
