3ddfe4fd86dd96f821d1dee8e9eff3768c65e0fd
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-05 00:04:21 -0300

    76th commit

--- calendar_01.py
+++ calendar_01.py
@@ -8,6 +8,14 @@
 # - Trabalhar com coisas específicas de calendários (ex.: calendar, month)
 # Por padrão dia da semana começa em 0 até 6
 # 0 = segunda-feira | 6 = domingo
+# locale para internacionalização (tradução)
+# https://docs.python.org/3/library/locale.html
+# https://learn.microsoft.com/fr-fr/powershell/module/international/get-winsystemlocale?view=windowsserver2022-ps&viewFallbackFrom=win10-ps
+
+import calendar
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
 import calendar
 
 print(calendar.calendar(2022))
