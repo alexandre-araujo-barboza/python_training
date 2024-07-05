1897e76c87c122d1177dcd5bd8bf0a656745f436
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-05 15:14:32 -0300

    86th commit

--- subprocess_01.py
+++ subprocess_01.py
@@ -31,14 +31,14 @@ encoding = 'utf_8'
 system = sys.platform
 
 if system == "win32":
-    cmd = ['ping', '127.0.0.1']
-    encoding = 'cp850'
+  cmd = ['ping', '127.0.0.1']
+  encoding = 'cp850'
 
 
 proc = subprocess.run(
-    cmd, capture_output=True,
-    text=True, encoding=encoding,
-    shell=True,
+  cmd, capture_output=True,
+  text=True, encoding=encoding,
+  shell=True,
 )
 
 print()
