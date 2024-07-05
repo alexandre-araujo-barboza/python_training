ff535eb18a180232a993a4fdfbf49ea5146fd819
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-05 14:36:28 -0300

    86th commit

--- Selenium/main.py
+++ Selenium/main.py
@@ -6,6 +6,10 @@ from pathlib import Path
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Caminho para a raiz do projeto
 ROOT_FOLDER = Path(__file__).parent
@@ -32,8 +36,22 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
   return browser
 
 if __name__ == '__main__':
+  TIME_TO_WAIT = 10
   # options = '--headless', '--disable-gpu',
   options = ()
   browser = make_chrome_browser(*options)
   browser.get('https://www.google.com')
-  sleep(5)
+
+  # Espere para encontrar o input
+  search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+    EC.presence_of_element_located(
+      (By.NAME, 'q')
+    )
+  )
+  search_input.send_keys('Hello World!')
+  search_input.send_keys(Keys.ENTER)
+  results = browser.find_element(By.ID, 'search')
+  links = results.find_elements(By.TAG_NAME, 'a')
+  links[0].click()
+  
+  sleep(TIME_TO_WAIT)
