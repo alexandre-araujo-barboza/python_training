0b45dc058f4ed1363ef254d53bbac708ccf30db1
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-06 11:42:49 -0300

    111th commit

--- QT6/Calculadora/display.py
+++ QT6/Calculadora/display.py
@@ -4,13 +4,14 @@ from PySide6.QtCore import Qt, Signal
 from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QLineEdit
 from variables import BIG_FONT_SIZE, MINIMUM_WIDTH, TEXT_MARGIN
-from utils import isEmpty
+from utils import isEmpty, isNumOrDot
 
 class Display(QLineEdit):
   eqPressed = Signal()
   delPressed = Signal()
   clearPressed = Signal()
-  
+  inputPressed = Signal(str)
+
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.configStyle()
@@ -51,4 +52,9 @@ class Display(QLineEdit):
     if isEmpty(text):
       return event.ignore()
 
-    print('Texto', text)
+    if isNumOrDot(text):
+      print(
+        'inputPressed pressionado, sinal emitido', type(self).__name__
+      )
+      self.inputPressed.emit(text)
+      return event.ignore()
