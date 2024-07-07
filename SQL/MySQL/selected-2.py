b892a5b8143552e7b5683031d7ca2c5f79b3caf5
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-07 06:38:23 -0300

    123th commit

---
+++ SQL/MySQL/selected-2.py
@@ -0,0 +1,24 @@
+import os
+import dotenv
+import pymysql
+import pymysql.cursors
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+  host = os.environ['MYSQL_HOST'],
+  user = os.environ['MYSQL_USER'],
+  password = os.environ['MYSQL_PASSWORD'],
+  database = os.environ['MYSQL_DATABASE'],
+  charset = 'utf8mb4',
+  cursorclass = pymysql.cursors.DictCursor,
+)
+
+# Trocando o cursor para retornar dicionários
+# pymysql.cursors.DictCursor
+
+with connection.cursor() as cursor:
+  cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+  for row in cursor.fetchall():
+    print(row)
