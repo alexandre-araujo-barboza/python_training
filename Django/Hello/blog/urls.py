f6c3883bac17ad0636d1a36b046c10aa470eb6cd
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-07 14:23:55 -0300

    147th commit

--- Django/Hello/blog/urls.py
+++ Django/Hello/blog/urls.py
@@ -2,6 +2,6 @@ from blog import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
-    path('example/', views.example),
+    path('', views.index, name='blog'),
+    path('example/', views.example, name='blog_example'),
 ]
--- Django/Hello/home/urls.py
+++ Django/Hello/home/urls.py
@@ -2,6 +2,6 @@ from home import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
-    path('example/', views.example),
+    path('', views.index, name='home'),
+    path('example/', views.example, name='home_example'),
 ]
