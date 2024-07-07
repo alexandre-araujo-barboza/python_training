0a1b0dcefbe12a9bd3625eb17f4f8ee1d54728fb
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-07 19:06:21 -0300

    162th commit

--- Django/Hello/project/settings.py
+++ Django/Hello/project/settings.py
@@ -123,6 +123,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base' / 'static'
 ]
+
+# Static files from production mode
+# https://whitenoise.readthedocs.io/en/latest/
+STATIC_ROOT = BASE_DIR / 'assets'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
