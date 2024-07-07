4ec25c6ced489fcc795c919afbcd1cde5b27ac59
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-07 06:15:28 -0300

    119th commit

---
+++ SQL/MySQL/selected.py
@@ -0,0 +1,33 @@
+import os
+import dotenv
+import pymysql
+
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+  host = os.environ['MYSQL_HOST'],
+  user = os.environ['MYSQL_USER'],
+  password = os.environ['MYSQL_PASSWORD'],
+  database = os.environ['MYSQL_DATABASE'],
+  charset = 'utf8mb4'
+)
+with connection:
+
+  # Lendo os valores com SELECT
+  with connection.cursor() as cursor:
+    menor = int(input('Digite a menor idade: '))
+    maior = int(input('Digite a maior idade: '))
+    
+    sql = (
+      f'SELECT * FROM {TABLE_NAME} '
+       'WHERE idade BETWEEN %s AND %s  '
+    )
+    
+    cursor.execute(sql, (menor, maior))
+    print(cursor.mogrify(sql, (menor, maior)))
+    data = cursor.fetchall()
+    
+    for row in data:
+      print(row)
