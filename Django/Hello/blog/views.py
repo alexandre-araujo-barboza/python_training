ce90213f1958867596178c2be20b3f5233103752
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-07 13:47:15 -0300

    145th commit

--- Django/Hello/blog/views.py
+++ Django/Hello/blog/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 
 def index(request):
+    context = {
+        'text' : 'Say hello page at blog!'
+    }
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 def example(request):
+    context = {
+        'text' : 'Say hello page at blog example!'
+    }
     return render(
         request,
-        'blog/example.html'
+        'blog/example.html',
+        context
     )
 
 
--- Django/Hello/home/views.py
+++ Django/Hello/home/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 
 def index(request):
+    context = {
+      'text' : 'Say hello page at home!'
+    }
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
 
 def example(request):
+    context = {
+        'text' : 'Say hello page at home example!'
+    }
     return render(
         request,
-        'home/example.html'
+        'home/example.html',
+        context
     )
 
