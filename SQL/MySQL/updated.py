d5479432e465c68260044404a2e2f7b1f2a24dab
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-07 06:29:52 -0300

    122th commit

---
+++ SQL/MySQL/updated.py
@@ -0,0 +1,31 @@
+import os
+import dotenv
+import pymysql
+
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+  host = os.environ['MYSQL_HOST'],
+  user = os.environ['MYSQL_USER'],
+  password = os.environ['MYSQL_PASSWORD'],
+  database = os.environ['MYSQL_DATABASE'],
+  charset = 'utf8mb4'
+)
+
+with connection.cursor() as cursor:
+
+  # Editando com UPDATE, WHERE e placeholders no PyMySQL
+  sql = (
+    f'UPDATE {TABLE_NAME} '
+     'SET nome=%s, idade=%s '
+     'WHERE id=%s'
+  )
+  cursor.execute(sql, ('Eleonor', 102, 4))
+  connection.commit()
+
+  cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+  for row in cursor.fetchall():
+    print(row)
+
