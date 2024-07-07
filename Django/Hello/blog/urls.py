b4d22cdac4ba34f82595d96d1905d9c5a0f02c37
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-07 17:30:01 -0300

    153th commit

--- Django/Hello/blog/urls.py
+++ Django/Hello/blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='blog'),
     path('example/', views.example, name='blog_example'),
-    path('post/<int:id>', views.post, name='post'),
+    path('post/<int:id>/', views.post, name='post'),
 ]
