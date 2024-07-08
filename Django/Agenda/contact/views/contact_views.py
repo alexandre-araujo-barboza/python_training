7a0be1c94362862855b86ddceb10aa7621dac33a
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-08 15:34:57 -0300

    199th commit

--- Django/Agenda/contact/views/contact_views.py
+++ Django/Agenda/contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 def index(request):
     contacts = Contact.objects \
@@ -16,9 +17,34 @@ def index(request):
         'contact/index.html',
         context
     )
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
 
 def contact(request, contact_id):
-    # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
