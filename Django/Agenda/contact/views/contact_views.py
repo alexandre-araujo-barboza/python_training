62afec16f16080193d1286e08ad1066c768d224e
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-08 16:12:56 -0300

    202th commit

--- Django/Agenda/contact/views/contact_views.py
+++ Django/Agenda/contact/views/contact_views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': ' - Contatos',
     }
 
