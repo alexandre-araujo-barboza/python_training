45d79568a7de412c782c580c8fee2995d02bf878
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-08 14:04:51 -0300

    192th commit

--- Django/Agenda/contact/admin.py
+++ Django/Agenda/contact/admin.py
@@ -10,7 +10,7 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
 
 @admin.register(models.Category)
