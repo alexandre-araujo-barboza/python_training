fdbded2de7968fbe3d0235f2115c99b0d4fb2462
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-09 15:17:32 -0300

    222th commit

--- Django/Agenda/contact/views/contact_forms.py
+++ Django/Agenda/contact/views/contact_forms.py
@@ -37,7 +37,9 @@ def create(request):
 
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact,
+        pk=contact_id,
+        show=True
     )
     form_action = reverse('contact:update', args=(contact_id,))
 
