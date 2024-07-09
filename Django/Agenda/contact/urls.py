c1f7db51bccaaf8914c65d3e8dc0bb9d79fe74ca
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-09 15:28:38 -0300

    223th commit

--- Django/Agenda/contact/urls.py
+++ Django/Agenda/contact/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     
     path('<int:contact_id>/', views.contact, name='contact'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
