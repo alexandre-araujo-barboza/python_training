4625c90984797a08f49e07e4b730954fd29d6af7
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-12 18:55:51 -0300

    284th commit

--- Django/Blog/app/setup/context_processors.py
+++ Django/Blog/app/setup/context_processors.py
@@ -1,11 +1,9 @@
 from setup.models import SiteSetup
 
-
 def context_processor_example(request):
     return {
         'example': 'Veio do context processor (example)'
     }
-
 
 def site_setup(request):
     setup = SiteSetup.objects.order_by('-id').first()
