e0cb8dfd343a88184ce22842b3ec2795888d32cc
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-15 20:48:34 -0300

    349th commit

--- Django/E-Commerce/order/admin.py
+++ Django/E-Commerce/order/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, ItemOrder
 
-# Register your models here.
+class ItemOrderInLine(admin.TabularInline):
+    model = ItemOrder
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemOrderInLine, 
+    ]
+    
+admin.site.register(Order, OrderAdmin)
+admin.site.register(ItemOrder)
