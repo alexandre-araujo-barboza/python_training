b119f7dae73a12d5141a1433eaaf08923595b178
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-15 19:34:31 -0300

    347th commit

--- Django/E-Commerce/product/admin.py
+++ Django/E-Commerce/product/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Product, Variation
 
-admin.site.register(Product)
+class VariationInLine(admin.TabularInline):
+    model = Variation
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        VariationInLine, 
+    ]
+    
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation)
