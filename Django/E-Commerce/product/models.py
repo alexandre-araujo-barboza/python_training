66e5a42e5740c297612f5bc3ab977b751287f8c0
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-15 19:09:45 -0300

    344th commit

--- Django/E-Commerce/product/models.py
+++ Django/E-Commerce/product/models.py
@@ -21,13 +21,21 @@ class Product(models.Model):
     )
     
     @staticmethod
-    def resize_image(img, new_with=800):
+    def resize_image(img, new_width=800):
         img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
         img_pil = Image.open(img_full_path)
         original_width, original_height = img_pil.size
-        print("Tamanho da imagem:") 
-        print(original_width, original_height) 
-        print("==================")
+        if original_width <= new_width:
+            img_pil.close()
+            return
+        new_height = round((new_width * original_height) / original_width)
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50,
+        )
+     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
