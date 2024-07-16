1cd248940a4d0b195daa4c7e3e77b4c5f8eb45e5
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-16 19:31:17 -0300

    357th commit

--- Django/E-Commerce/pedido/models.py
+++ Django/E-Commerce/pedido/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pedido(models.Model):
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+    user   = models.ForeignKey(User, on_delete=models.CASCADE)
+    total  = models.FloatField()
+    status = models.CharField(
+        default="C",
+        max_length=1,
+        choices=(
+            ('C', 'Criado'),
+            ('P', 'Pendente'),
+            ('A', 'Aprovado'),
+            ('R', 'Reprovado'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        )
+    )
+
+    def __str__(self):
+        return f'Pedido Nro. {self.pk}' 
+    
+class ItemPedido(models.Model):
+    class Meta:
+        verbose_name = 'Item do Pedido'
+        verbose_name_plural = 'Itens do Pedido'
+
+    pedido      = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto    = models.CharField(max_length=255)
+    produto_id = models.PositiveIntegerField()
+    variacao  = models.CharField(max_length=255)
+    variacao_id = models.PositiveIntegerField()
+    preco        = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    quantidade = models.PositiveIntegerField()
+    imagem  = models.CharField(max_length=2000) 
+
+    def __str__(self):
+        return f'Detalhes do pedido Nro. {self.order}' 
