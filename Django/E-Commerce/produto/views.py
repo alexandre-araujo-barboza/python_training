1cabe559eba58fdef8b31daab7a0d5d0564cd81b
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-18 18:52:06 -0300

    388th commit

--- Django/E-Commerce/produto/views.py
+++ Django/E-Commerce/produto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
@@ -20,11 +20,33 @@ class ProductDetails(DetailView):
 class ProductAddToCart(View):
     
     def get(self, *args, **kargs):
-        messages.success(
-            self.request,
-            'mensagem de sucesso'
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('produto:lista')
         )
-        return redirect(self.request.META['HTTP_REFERER'])
+        variacao_id = self.request.GET.get('vid')
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto não existe'
+            )
+            return redirect(http_referer)
+        variacao = get_object_or_404(models.Variacao, id = variacao_id)
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {
+
+            }
+            self.request.session.save()
+
+        carrinho = self.request.session['carrinho']
+        if variacao_id in carrinho:
+            # variação existe no carrinho
+            pass
+        else:
+            # variação não existe no carrinho
+            pass
+            
+        return HttpResponse(f'{variacao.produto} {variacao.nome}')
 class ProductRemoveFromCart(View):
 
     def get(self, *args, **kargs):
