3ef43a6ec8afdf18dac5df23403d6c7860a8cb3b
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-17 21:23:28 -0300

    368th commit

--- Django/E-Commerce/produto/views.py
+++ Django/E-Commerce/produto/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from . import models
 
 class ProductsList(ListView):
-
-    def get(self, *args, **kargs):
-        return HttpResponse('listar')
-
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    
 class ProductDetails(View):
 
     def get(self, *args, **kargs):
