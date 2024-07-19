f25b8292703beefe0ab678f173612b70b9242ff6
alexandre-araujo-barboza <alexandre.araujo.barboza@gmail.com>
2024-07-18 23:29:50 -0300

    401th commit

--- Django/E-Commerce/utils/onfilters.py
+++ Django/E-Commerce/utils/onfilters.py
@@ -5,3 +5,13 @@ register = Library()
 @register.filter
 def price_format(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+@register.filter
+def product_sum(carrinho):
+    count = 0
+    for product in carrinho:
+      if product['preco_quantitativo_promocional']:
+          count += product['preco_quantitativo_promocional']
+      else:
+          count += product['preco_quantitativo']
+    return count 
